c1d2c4bac48921bd0c7d962bc13910982331e59e
Schmidty909 <matthew.schmidty@aol.com>
2022-10-17 23:41:15 -0400

    Working insert script

--- SQL Scripts/counts_and_minus_function.sql
+++ SQL Scripts/counts_and_minus_function.sql
@@ -5,23 +5,13 @@ From netflix;
 Select Count(*) As disney_count	# 1448
 From disney;
 
-Select Count(*) As hulu_count	# 2594
+Select Count(*) As hulu_count	# 2593
 From hulu;
 
-Select Count(*) As prime_count	# 9615
+Select Count(*) As prime_count	# 9598
 From prime;
 
-Select Count(*) As total	# 22,442
+Select Count(*) As total	# 22,425
 From streaming;
 
-# Total Rows = 21,791
-
-# Finding the movies that match
-Select Count(d.title) as Same	# 45
-From disney d
-Join streaming s ON d.title = s.title;
-
-Select Count(d.title) as different	# 1403
-From disney d
-LEFT JOIN streaming s ON d.title = s.title
-Where s.title IS NULL;
+# Total Rows = 22425
