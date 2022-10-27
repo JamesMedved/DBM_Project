540e319a2c88c392ac9b0ebece0adf23d83852ef
JamesMedved <90870165+JamesMedved@users.noreply.github.com>
2022-10-27 01:40:27 -0400

    basic webpage with search bar

--- website/mysite/polls/views.py
+++ website/mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def homepage(request):
+    return render(request, 'homepage.html')
