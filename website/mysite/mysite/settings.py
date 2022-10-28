c1c42c73fcf321b84b2b2909bb1b3db1fdee1553
JamesMedved <90870165+JamesMedved@users.noreply.github.com>
2022-10-28 18:15:05 -0400

    connected to server

--- website/mysite/mysite/settings.py
+++ website/mysite/mysite/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbm_prj',
+        'USER': 'root',
+        'PASSWORD': 'RooT1234@@',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
