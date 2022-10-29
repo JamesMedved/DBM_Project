e474955997eb491528e500092c7d3148d0a37bab
JamesMedved <90870165+JamesMedved@users.noreply.github.com>
2022-10-29 19:58:42 -0400

    search done for demo

--- website/mysite/polls/views.py
+++ website/mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Titles
+
 
 # Create your views here.
 def search(request):
@@ -8,7 +10,7 @@ def search(request):
     search = request.POST.get('search', '')
     # Search for title if input is not empty
     if request.method == "POST" and search:
-        title_set = Titles.objects.filter(name__icontains=search)
+        title_set = Titles.objects.filter(Q(name__icontains=search) | Q(cast__icontains=search) | Q(director__icontains=search))
         return render(request, 'search.html', {'title_set': title_set, 'search':search})
     else:
         return render(request, 'search.html', {})
