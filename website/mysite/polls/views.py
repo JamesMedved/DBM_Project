6dd575574c2a2da3d7736cb1df4a99a385814cd7
JamesMedved <90870165+JamesMedved@users.noreply.github.com>
2022-10-29 18:33:31 -0400

    fix search post

--- website/mysite/polls/views.py
+++ website/mysite/polls/views.py
@@ -5,11 +5,10 @@ from .models import Titles
 # Create your views here.
 def search(request):
     # Grab search input
-    searched = request.POST['searched']
+    searched = request.POST.get('search', '')
     # Search for title if input is not empty
     if request.method == "POST" and searched:
-        title_set = Titles.objects.filter(name__icontains=searched)
-        return render(request, 'search.html', {'title_set': title_set})
+        return render(request, 'search.html', {'title_set': Titles.objects.filter(name__icontains=searched)})
     else:
         return render(request, 'search.html', {})
 
