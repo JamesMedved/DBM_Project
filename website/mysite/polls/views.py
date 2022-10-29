0e6f31a102bd2561309f4f131369a19e6863881d
JamesMedved <90870165+JamesMedved@users.noreply.github.com>
2022-10-29 18:51:30 -0400

    add ''no results found" error

--- website/mysite/polls/views.py
+++ website/mysite/polls/views.py
@@ -5,10 +5,11 @@ from .models import Titles
 # Create your views here.
 def search(request):
     # Grab search input
-    searched = request.POST.get('search', '')
+    search = request.POST.get('search', '')
     # Search for title if input is not empty
-    if request.method == "POST" and searched:
-        return render(request, 'search.html', {'title_set': Titles.objects.filter(name__icontains=searched)})
+    if request.method == "POST" and search:
+        title_set = Titles.objects.filter(name__icontains=search)
+        return render(request, 'search.html', {'title_set': title_set, 'search':search})
     else:
         return render(request, 'search.html', {})
 
