80a191f9199e99f4d60d78c800a931654f3df27c
Schmidty909 <matthew.schmidty@aol.com>
2022-10-30 03:30:57 -0400

    Authentication complete

--- website/mysite/polls/views.py
+++ website/mysite/polls/views.py
@@ -6,11 +6,13 @@ from .models import WatchLater
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 from .forms import CreateUserForm
 
 # Create your views here.
+@login_required(login_url='loginPage')
 def search(request):
     # Grab search input
     search = request.POST.get('search', '')
@@ -21,35 +23,42 @@ def search(request):
     else:
         return render(request, 'search.html', {})
 
+@login_required(login_url='loginPage')
 def watch_later(request):
     title_set = WatchLater.objects.all()
     return render(request, 'watch_later.html', {'title_set': title_set})
 
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('search')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('loginPage')
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('loginPage')
 
-    context = {'form':form}
-    return render(request, 'register.html', context)
+        context = {'form':form}
+        return render(request, 'register.html', context)
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('search')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('search')
-        else:
-            messages.info(request, 'Username or password is incorrect')
-    context = {}
-    return render(request,'login.html', context)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('search')
+            else:
+                messages.info(request, 'Username or password is incorrect')
+        context = {}
+        return render(request,'login.html', context)
 
 def logoutUser(request):
     logout(request)
