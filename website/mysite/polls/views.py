c0c0ec0fae5d2c30f62bde1474c2f712a6f39266
JamesMedved <90870165+JamesMedved@users.noreply.github.com>
2022-10-30 02:02:30 -0400

    trying to get button working

--- website/mysite/polls/views.py
+++ website/mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.db.models import Q
 from .models import Titles
 from .models import WatchLater
@@ -7,14 +7,22 @@ from .models import WatchLater
 
 # Create your views here.
 def search(request):
-    # Grab search input
-    search = request.POST.get('search', '')
+    search = request.POST.get('search')
     # Search for title if input is not empty
-    if request.method == "POST" and search:
-        title_set = Titles.objects.filter(Q(name__icontains=search) | Q(cast__icontains=search) | Q(director__icontains=search))
-        return render(request, 'search.html', {'title_set': title_set, 'search':search})
-    else:
-        return render(request, 'search.html', {})
+    if request.method == "POST":
+        if search:
+            title_set = Titles.objects.filter(Q(name__icontains=search) | Q(cast__icontains=search) | Q(director__icontains=search))
+            return render(request, 'search.html', {'title_set': title_set, 'search':search})
+    return render(request, 'search.html', {})
+
+def add_watch_later(request):
+    add_wl_title_id = request.POST.get('add_wl')
+    # Search for title if input is not empty
+    if request.method == "POST":
+        if add_wl_title_id:
+            # wl = WatchLater(title_id=1, user_id=1)
+            # wl.save()
+            return render(request, 'search.html', {'wl':add_wl_title_id})
 
 def watch_later(request):
     title_set = WatchLater.objects.all()
